1d3b46bf14beacaee945378d1dcf306e280eb8dd
edgarmejiav <legamejiav@gmail.com>
2024-07-15 16:10:36 -0500

    feat: video

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/download', (req, res) => {
     const videoURL = req.body.url;
@@ -35,43 +34,18 @@ app.post('/download', (req, res) => {
         res.status(500).json({ success: false, message: 'Error al obtener la información del video' });
     });
 });
+app.use('/videos', express.static(path.join(__dirname, 'videos')));
+
+// Ruta para manejar la solicitud del video
 app.get('/video', (req, res) => {
-    const videoPath = path.join(__dirname, 'Shawn_Mendes___There_s_Nothing_Holdin__Me_Back__Official_Music_Video_.mp4');
-    const stat = fs.statSync(videoPath);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-
-    if (range) {
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-        if(start >= fileSize) {
-            res.status(416).send('Requested range not satisfiable\n' + start + ' >= ' + fileSize);
-            return;
-        }
-
-        const chunksize = (end - start) + 1;
-        const file = fs.createReadStream(videoPath, { start, end });
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(videoPath).pipe(res);
-    }
+    // Ruta al video que deseas enviar
+    const videoPath = path.join(__dirname, 'videos', 'Shawn_Mendes___There_s_Nothing_Holdin__Me_Back__Official_Music_Video_.mp4.webm');
+    console.log(videoPath)
+    res.sendFile(videoPath);
 });
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
