19f5a1507a4e9fc4030b294a0b91561f6a78425c
edgarmejiav <legamejiav@gmail.com>
2024-07-18 12:30:46 -0500

    feat: download

--- server.js
+++ server.js
@@ -16,25 +16,26 @@ app.post('/download', (req, res) => {
     if (!videoURL) {
         return res.status(400).json({ success: false, message: 'URL no proporcionada' });
     }
+    res.json({ success: true, title: res.body.url })
 
-    youtubedl(videoURL, { dumpSingleJson: true }).then(info => {
-        const title = info.title.replace(/[^a-zA-Z0-9]/g, '_');
-        const output = path.join(__dirname, 'videos', `${title}.mp4`);
-
-        youtubedl(videoURL, {
-            output: output,
-            // format: 'bestvideo+bestaudio'
-            format: 'worst'
-        }).then(() => {
-            res.json({ success: true, title: title });
-        }).catch(err => {
-            console.error('Error durante la descarga:', err);
-            res.status(500).json({ success: false, message: 'Error durante la descarga' });
-        });
-    }).catch(err => {
-        console.error('Error al obtener la información del video:', err);
-        res.status(500).json({ success: false, message: 'Error al obtener la información del video' });
-    });
+    // youtubedl(videoURL, { dumpSingleJson: true }).then(info => {
+    //     const title = info.title.replace(/[^a-zA-Z0-9]/g, '_');
+    //     const output = path.join(__dirname, 'videos', `${title}.mp4`);
+    //
+    //     youtubedl(videoURL, {
+    //         output: output,
+    //         // format: 'bestvideo+bestaudio'
+    //         format: 'worst'
+    //     }).then(() => {
+    //         res.json({ success: true, title: title });
+    //     }).catch(err => {
+    //         console.error('Error durante la descarga:', err);
+    //         res.status(500).json({ success: false, message: 'Error durante la descarga' });
+    //     });
+    // }).catch(err => {
+    //     console.error('Error al obtener la información del video:', err);
+    //     res.status(500).json({ success: false, message: 'Error al obtener la información del video' });
+    // });
 });
 
 app.get('/video', (req, res) => {
@@ -55,6 +56,25 @@ app.get('/video', (req, res) => {
         }
     });
 });
+app.get('/delete', (req, res) => {
+    const name = req.query.name; // Obtén el nombre del video desde la consulta (?name=nombre)
+    if (!name) {
+        return res.status(400).send('Nombre del video no especificado');
+    }
+
+    const videoPath = path.join(__dirname, 'videos', name + ".mp4");
+
+    console.log(videoPath)
+    fs.unlink(videoPath, (err) => {
+        if (err) {
+            console.error('Error al eliminar el video:', err.message);
+            res.status(err.status).end();
+        } else {
+            console.log(`Video eliminado correctamente: ${videoPath}`);
+        }
+    })
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
